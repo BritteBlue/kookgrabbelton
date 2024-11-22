1007ebf1c230e1a3caeab35f02900ea590e606d2
BritteBlue <britteschuur@kpnmail.nl>
2024-11-22 05:50:43 +0100

    add home page

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Recepten Ranking</h1>
+      <p>Welkom bij de Recepten Grabbelton Rankings!</p>
+      <p className="mt-2">Recept van de maand: Arayes</p>
+    </main>
+  );
+}
