c2a0afd89a52639dc8e7df0155e24502bbefd1e1
BritteBlue <britteschuur@kpnmail.nl>
2024-11-23 09:17:53 +0100

    Update page.js

--- app/recept/page.js
+++ app/recept/page.js
@@ -1,3 +1,6 @@
+"use client"
+import React, { useState } from 'react';
+
 import React, { useState } from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Star } from 'lucide-react';
